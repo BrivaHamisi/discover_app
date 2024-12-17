3bb1d1e6dc28f14966119339e510ec6e11d4e28e
BrivaHamisi <brivahamisi@gmail.com>
2024-12-17 15:45:42 +0300

    Top Bar

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,24 +1,59 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget{
+class HomePage extends StatefulWidget {
   @override
+  State<StatefulWidget> createState() {
+    return _HomepageState();
+  }
+}
+
+class _HomepageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   title: Text('Home Page'),
-      // ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('Welcome to the Home Page!'),
-            ElevatedButton(
-              onPressed: () => {},
-              child: Text('About'),
-            ),
-          ],
-        ),
-      ),
+      body: SafeArea(
+        top: true,
+        left: false,
+        right: false,
+        bottom: true,
+        minimum: EdgeInsets.fromLTRB(MediaQuery.of(context).size.width * 0.05, 0, MediaQuery.of(context).size.width * 0.05, 0),
+
+        child: _mainColmn(),)
     );
+  }
+
+  Widget  _mainColmn(){
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      mainAxisSize: MainAxisSize.max,
+      crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.menu,
+                size: 35,
+                color: Colors.black,
+              ),
+              Container(
+                height: 39,
+                width: 144,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      fit: BoxFit.fill,
+                      image: AssetImage('assets/images/logo_discover.png')),
+                ),
+              ),
+              Icon(
+                Icons.search,
+                size: 35,
+                color: Colors.black,
+              ),
+            ],
+          )
+        ]
+      );
   }
 }
